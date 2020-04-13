8ba63331d9c1892939efa4514165b39063f198a5
darh0ky <jgamerino@gmail.com>
2020-04-12 22:15:58 -0400

    adding signup/login

---
+++ lingo-labs/src/components/home.js
@@ -0,0 +1,20 @@
+import React, { Component } from 'react'
+
+
+class Home extends Component {
+    constructor() {
+        super()
+    }
+
+    render() {
+      
+        return (
+            <div id="title">
+                <h1>Welcome to Lingo Labs</h1>
+            </div>
+        )
+
+    }
+}
+
+export default Home
