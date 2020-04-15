9cc4de32a2c206acd2a7ecd1d496900c3d674207
nikola <nikola4work@gmail.com>
2020-04-14 21:24:24 -0400

    added navbar,home-page,logIn&SignIn modal, LingoLab logo

---
+++ lingolabs/src/Components/Footer.js
@@ -0,0 +1,44 @@
+import React from 'react';
+import "./style/footer.css"
+import Container from "react-bootstrap/Container";
+import Row from "react-bootstrap/Row";
+import Col from "react-bootstrap/Col";
+
+function Footer(){
+    return (
+      <Container fluid className="footer">
+        <Row>
+          <Col md={12}>
+            <h3 className="socialMedia">Find us on Social Media</h3>
+            <a href="#">
+              <i class="fa fa-facebook-square fa-3x" id="sm"></i>
+            </a>
+            <a href="#">
+              <i class="fa fa-instagram fa-3x" id="sm"></i>
+            </a>
+            <a href="#">
+              <i class="fa fa-twitter-square fa-3x" id="sm"></i>
+            </a>
+          </Col>
+        </Row>
+
+        <Col md={4}>
+          <address>
+            Phone: (202)202-2020 <br />
+            Visit us at: 1600 Pennsylvania Ave NW <br /> Washington <br /> DC
+            20500 Box 564
+          </address>
+        </Col>
+
+        <Col md={4}>
+          <p className="copy">
+            {" "}
+            &copy; All rights reserved to Lingo Lab
+            <br /> & <br /> Team Members{" "}
+          </p>{" "}
+        </Col>
+      </Container>
+    );
+}
+
+export default Footer;
---
+++ lingolabs/src/Components/Templates.js
@@ -0,0 +1,8 @@
+import React from "react";
+import "../Components/style/templates.css";
+
+function Templates() {
+  return <h1 className="niko">Templates work</h1>;
+}
+
+export default Templates;
